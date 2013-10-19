49c8b5a6dfae2eb133838777c6646fbe3fe9fe6e
Daher Alfawares <daher.alfawares@live.com>
2013-10-19 11:40:13 -0500

    adding stories to storyboard

---
+++ date-create/date-create/FinalDateViewController.h
@@ -0,0 +1,13 @@
+//
+//  FinalDateViewController.h
+//  date-create
+//
+//  Created by Daher Alfawares on 10/19/13.
+//  Copyright (c) 2013 Daher Alfawares. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FinalDateViewController : UIViewController
+
+@end
---
+++ date-create/date-create/FinalDateViewController.m
@@ -0,0 +1,38 @@
+//
+//  FinalDateViewController.m
+//  date-create
+//
+//  Created by Daher Alfawares on 10/19/13.
+//  Copyright (c) 2013 Daher Alfawares. All rights reserved.
+//
+
+#import "FinalDateViewController.h"
+
+@interface FinalDateViewController ()
+
+@end
+
+@implementation FinalDateViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
