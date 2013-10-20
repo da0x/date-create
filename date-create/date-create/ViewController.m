e5a19b26657e419e521b60d45466429589a66675
Daher Alfawares <daher.alfawares@live.com>
2013-10-20 13:14:05 -0500

    no message

--- date-create/date-create/ViewController.m
+++ date-create/date-create/ViewController.m
@@ -22,7 +22,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    [self.navigationItem setTitle:@"Jill Solzman"];
+    [self.navigationItem setTitle:@"Jill Salzman"];
 }
 
 - (void)didReceiveMemoryWarning
